30294ba7f37bce444f5f6435d1e7c82e4723076c
Jonathan Bramble <j.bramble@sheffield.ac.uk>
2014-06-23 17:22:21 +0100

    updated cli commands

--- lib/serialbox/cli.rb
+++ lib/serialbox/cli.rb
@@ -11,23 +11,20 @@ module Serialbox
     		File.dirname(__FILE__)
   		end
 
-		desc "new NAME", "create a new serialbox project"	
-		method_option :adapter, :aliases => "-d", :desc => "set the adapter"	
+  		option :adapter, aliases: "-d", desc: "set the adapter type", required: true	
+		desc "new NAME", "create a new serialbox project"
   		def new(name)
     		say "creating a new serialbox project #{name}", :green
     		# create the directory and the classes but will need some kind of config options 
-    		
-    		puts destination_root
-    		#copy_file "generators/README", "#{name}/README"
-    		#invoke readme(name)
-  		end
+    		readme(name)
 
-  		desc "readme NAME", "create a readme file"
-  		def readme(name)
-  			#create readme file and insert the contents
-  			str = "text to read"
-  			copy_file "generators/README", "#{name}/README"
-  			insert_into_file "#{name}/README", str
+    		case options[:adapter]
+    			when "mongoid"
+    				say 'Using mongoid adapter', :green
+    				insert_mongoid(name)
+
+    		end
+
   		end
 
   		desc "help", "serialbox help"
@@ -39,6 +36,26 @@ Description:  A simple app to create a database or file based storage adapter fo
 
   			DES
   			help
+  		end
+
+
+  		no_commands do
+ 			def readme(name)
+  				#create readme file and insert the contents
+  				str = "text to read"
+  				copy_file "generators/README", "#{name}/README"
+  				#insert_into_file "#{name}/README", str
+  			end
+
+  			def insert_mongoid(name)
+  				copy_file "generators/mongoid.yml", "#{name}/mongoid.yml"
+  				#append instructions to readme
+  			end
+
+  			#when developed
+  			def insert_file_system
+  				#append instructions to readme
+  			end
   		end
 
 
