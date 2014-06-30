6de3ba392cc23d27db3c2697fac9cd3a1515c089
Jonathan Bramble <j.bramble@sheffield.ac.uk>
2014-06-30 17:32:12 +0100

    updates to includes

--- lib/serialbar.rb
+++ lib/serialbar.rb
@@ -1,6 +1,8 @@
 require 'serialbar/version'
 require 'serialbar/exceptions'
+require 'serialbar/generators/boiler_plate'
 require 'serialbar/cli'
 require 'serialbar/listener'
 require 'serialbar/helpers'
 require 'serialbar/adapter'
+
